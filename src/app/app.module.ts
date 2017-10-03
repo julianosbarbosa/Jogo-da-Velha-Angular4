6f5a6c21999590a68e5021339cccb46a75e3a311
Juliano <juliano@nationphone.com>
2017-10-03 19:38:24 -0300

    UPDATE -  Configuração do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { JogoDaVelhaModule } from './jogo-da-velha';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    JogoDaVelhaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
